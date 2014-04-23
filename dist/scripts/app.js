ca6d2140d1bfc8b7446820d749f63094d7504275
Jorge Valverde <horuxd@gmail.com>
2014-04-23 14:07:52 -0600

    Fluid Math convertion - finished

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -13,3 +13,41 @@ app.config(function($routeProvider){
 			redirectTo : '/math'
 		})
 });
+
+app.controller('emConvertion',function($scope){
+	$scope.base = 16;
+
+	var convertionType = 'px-to-em',
+		convert = {
+			'px-to-em' : function(){
+				if( Number($scope.px) && Number($scope.base)){
+					return ( ($scope.px/$scope.base).toFixed(4).replace(/0{0,2}$/, "") + 'em');
+				}
+			},
+			'em-to-px' : function(){
+				if( Number($scope.em) && Number($scope.base)){
+					return ( Math.floor($scope.em*$scope.base) + 'px');
+				}
+			}
+		};
+
+	$scope.clear = function(){ $scope.px = ''; $scope.em = ''};
+	$scope.clearBase = function(){ $scope.base = ''; };
+	$scope.changeConvertionType = function($event){ convertionType = $event.target.getAttribute('data-convertionType'); }
+	$scope.pixelConvertion = function(){
+		$scope.result = convert[convertionType]();
+	}
+
+});
+
+app.controller('percentConvertion',function($scope){
+	$scope.base = 960;
+
+	$scope.clear = function($event){ $event.target.value = ''; };
+	$scope.calculatePercent = function() {
+		if( Number($scope.px) && Number($scope.base)){
+			$scope.result = ($scope.px/$scope.base*100)+'%';
+		}
+	}
+
+});
